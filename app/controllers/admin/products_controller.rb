cd0b4ffc324876f067378b0b6974025c99469055
malborno2020 <manuel.albornoz@zoho.com>
2021-11-04 23:00:51 -0300

    agregando cruds stores

---
+++ app/controllers/admin/products_controller.rb
@@ -0,0 +1,46 @@
+class Admin::ProductsController < ApplicationController
+  def index
+    @products = Product.all
+  end
+
+  def new
+    @product = Product.new
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def create
+    @product = Product.new(params_product)
+    if @product
+      redirect_to admin_products_path
+    else
+      render  :new
+    end
+  end
+
+  def update
+    if @product.update(params_product)
+      redirect_to admin_products_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @product.destroy
+    redirect_to admin_products_path
+  end
+
+  private
+  def params_product
+    params.require(:product).permit(:name)
+  end
+
+  def set_product
+    @product = Product.find(params[:id])
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,31 @@
 Rails.application.routes.draw do
+  namespace :admin do
+    get 'products',               to:'products#index',   as: 'products'
+    get 'products/new',           to:'products#new',     as: 'new_product'
+    get 'products/:id',           to:'products#show',    as: 'product'
+    get 'products/:id/edit',      to:'products#edit',    as: 'edit_product'
+
+    post 'products',              to: 'products#create'
+    put 'products/:id',           to: 'products#update'    
+    patch 'products/:id',         to: 'products#update'   
+    delete 'products/edit',       to: 'products#destroy'
+  
+
+  end
+  namespace :admin do
+    get 'categories',             to: 'categories#index',   as: 'categories'
+    get 'categories/new',         to: 'categories#new',     as: 'new_category'
+    get 'categories/:id',         to: 'categories#show',    as: 'category'
+    get 'categories/:id/edit',    to: 'categories#edit',    as: 'edit_category'
+  
+    post 'categories',            to: 'categories#create'
+    put 'categories/:id',         to: 'categories#update'    
+    patch 'categories/:id',       to: 'categories#update'   
+    delete 'categories/edit',     to: 'categories#destroy'
+  
+  
+  
+  
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
